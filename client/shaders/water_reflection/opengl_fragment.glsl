a33a5ae16986c1b0ff6db8862564990c249c3348
GreenXenith <24834740+GreenXenith@users.noreply.github.com>
2023-09-17 01:30:05 -0700

    FRESNEL EFFECT AND BETTER RIPPLES

--- client/shaders/water_reflection/opengl_fragment.glsl
+++ client/shaders/water_reflection/opengl_fragment.glsl
@@ -3,13 +3,12 @@
 #define normalmap texture2
 #define water_mask texture3
 
+#define uv varTexCoord.st
+
 uniform sampler2D rendered;
-uniform sampler2D bloom;
 uniform sampler2D depthmap;
 uniform sampler2D normalmap;
 uniform sampler2D water_mask;
-
-uniform vec2 texelSize0;
 
 uniform vec4 skyBgColor;
 
@@ -43,11 +42,13 @@ vec3 worldPos(vec2 pos) {
     return position.xyz / position.w;
 }
 
+// Permute
 vec4 perm(vec4 x)
 {
 	return mod(((x * 34.0) + 1.0) * x, 289.0);
 }
 
+// Simplex noise
 float snoise(vec3 p)
 {
 	vec3 a = floor(p);
@@ -75,66 +76,82 @@ const float _MaxDistance = 100000.0;
 const float _Step = 0.05;
 const float _Thickness = 0.05;
 
+vec4 raycast(vec3 position, vec3 direction, float limit, vec4 fallback) {
+    float ray_length = length(position) * 0.05;
+    float start_depth = texture2D(depthmap, uv).x;
+    // float ray_length = _Step;
+
+    float stp = _Step;
+    vec3 march_position = position;
+
+    vec2 sample_uv;
+    float screen_depth, target_depth;
+
+    while (ray_length < limit) {
+        march_position = position + direction * ray_length;
+        sample_uv = projectPos(march_position);
+
+        if (sample_uv.x > 1 || sample_uv.x < 0 || sample_uv.y > 1 || sample_uv.y < 0) {
+            break;
+        }
+
+        screen_depth = worldPos(sample_uv).z;
+        target_depth = march_position.z;
+
+        // if ((screen_depth - target_depth) < 0.0005) {
+        // FIXME: Rays hitting floating obstructions
+        //  && march_position.z - screen_depth < stp * 5
+        if (texture2D(depthmap, sample_uv).x > start_depth && march_position.z / screen_depth > 1.01) {
+            return texture2D(rendered, sample_uv);
+        }
+
+        ray_length += stp;
+        stp *= 1.01;
+    }
+
+    return fallback;
+}
+
+const float _WindSpeed = 100.0;
+const vec3 _WindDir = normalize(vec3(1.0, 0.0, 1.0));
+
+const float _WaveWidth = 16.0;
+const float _WaveHeight = 0.02;
+
+const float _RippleWidth = 8.0;
+const float _RippleHeight = 0.02;
+
+const float _ReflectFactor = 0.5;
+
 void main(void) {
-	vec2 uv = varTexCoord.st;
-    vec2 render_uv = uv;
 	vec4 mask = texture2D(water_mask, uv);
-	// vec4 color = vec4(0.0);
-    vec4 color = texture2D(rendered, uv);
+    vec4 base_color = texture2D(rendered, uv);
 
     // if (mask == vec4(1.0)) { // This somehow catches the sun color ........... somehow
     if (mask == vec4(1.0, 0.0, 1.0, 1.0)) {
-        color = skyBgColor;
-
         vec3 position = worldPos(uv);
         vec3 normal = normalize(mat3(mCameraView) * texture2D(normalmap, uv).xyz);
-        vec4 real_pos = vec4(position, 1.0) * mCameraViewInv;
-        // normal.x += snoise((position * mat3(mCameraViewInv)) / 10) ;
-        // normal.x += (snoise((position * mat3(mCameraView) / 16 + animationTimer * 100) ) * 0.1) - 0.05;
 
-	// vec3 wavePos = (mWorld * pos).xyz + cameraOffset;
+        // Waves and ripples
+        vec3 real_position = position * mat3(mCameraView) + cameraPosition;
 
-	// wavePos.x /= WATER_WAVE_LENGTH * 3.0;
-	// wavePos.z /= WATER_WAVE_LENGTH * 2.0;
-	// wavePos.z += animationTimer * WATER_WAVE_SPEED * 10.0;
-	// pos.y += (snoise(wavePos) - 1.0) * WATER_WAVE_HEIGHT * 5.0;
+        float wave_noise = snoise((real_position / _WaveWidth) + (animationTimer * _WindSpeed * 0.5 * _WindDir));
+        float ripple_noise = snoise((real_position / _RippleWidth) + (animationTimer * _WindSpeed * 2.0 * _WindDir));
 
-        // normal.x += (snoise((position * mat3(mCameraView) + cameraPosition)));
-        normal.x += (snoise((vec3(position.x / 20, position.y, position.z / 20 + animationTimer * 100) * mat3(mCameraView)))) / 20;
+        normal.z += ((wave_noise - 0.5) * _WaveHeight) + ((ripple_noise - 0.5) * _RippleHeight);
 
-        vec3 ray_step = reflect(normalize(position), normal);
-        float ray_length = length(position) * 0.05;
-        float start_depth = texture2D(depthmap, uv).x;
-        // float ray_length = _Step;
+        // Reflection color
+        vec3 camera_dir = normalize(position);
+        vec3 direction = reflect(camera_dir, normal);
+        vec4 reflect_color = raycast(position, direction, _MaxDistance, skyBgColor);
 
-        float stp = _Step;
-        vec3 march_position = position;
+        // Fresnel
+        float factor = dot(-camera_dir, normal);
+        factor = pow(factor, _ReflectFactor);
 
-        vec2 sample_uv;
-        float screen_depth, target_depth;
-
-        while (ray_length < _MaxDistance) {
-            march_position = position + ray_step * ray_length;
-            sample_uv = projectPos(march_position);
-
-            if (sample_uv.x > 1 || sample_uv.x < 0 || sample_uv.y > 1 || sample_uv.y < 0) {
-                break;
-            }
-
-            screen_depth = worldPos(sample_uv).z;
-            target_depth = march_position.z;
-
-            // if ((screen_depth - target_depth) < 0.0005) {
-            if (texture2D(depthmap, sample_uv).x > start_depth && march_position.z / screen_depth > 1.01) {
-                color = texture2D(rendered, sample_uv);
-                // render_uv = sample_uv;
-                break;
-            }
-
-            ray_length += stp;
-            stp *= 1.01;
-        }
+        gl_FragColor = mix(reflect_color, base_color, factor);
+        return;
     }
 
-    gl_FragColor = color;
+    gl_FragColor = base_color;
 }
